ee17cc9722027cae86df179976841d6b17525a6c
Lucas Aho <aholucas11@gmail.com>
2019-11-16 12:45:43 -0700

    Widget added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ var weather;
 
 var resortZip = 80435; //change to button value with mike drop down
 $("#test").on("click", function() {
-    
+    $(".widget1Display") = "<div class='ots-widget'><iframe id='o28858' width='600' height='150' border='0' frameborder='0' scrolling='no' src='https://www.onthesnow.com/widget/list?resorts=" + resortId[4] + "&color=b'></iframe><p><a href='https://www.onthesnow.com' target='_blank'>Powered by OnTheSnow.com</a></p></div>";
     var openWeatherQueryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=" + resortZip + ",us&units=imperial&APPID=8945840c8250c919b4821b938074f3a6"
 
     // Ajax call using the open weather api
@@ -28,9 +28,7 @@ $("#test").on("click", function() {
     })
 }) 
 
-$("#test").on("click", function() {
-    $(".widget1Display") = "<div class='ots-widget'><iframe id='o28858' width='600' height='150' border='0' frameborder='0' scrolling='no' src='https://www.onthesnow.com/widget/list?resorts=" + resortId[4] + "&color=b'></iframe><p><a href='https://www.onthesnow.com' target='_blank'>Powered by OnTheSnow.com</a></p></div>";
-});
+
 
 /* Arapahoe Basin 20, Aspen Snowmass 25, Aspen Highlands 25, Aspen Mountain 25, Beaver Creek 36, Breckenridge 77, Buttermilk 25, 
 Copper 113, Crested Butte 120, echo mountain 1673, Eldora 143, GRANBY NO RESULT, Howelson 181, Keystone 197, Loveland 220, 
