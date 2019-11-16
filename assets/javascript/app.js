2a24039978d29ecee33fbedea209c4d27f5a98b2
Lucas Aho <aholucas11@gmail.com>
2019-11-16 11:27:48 -0700

    Temperature logged for midday one day away

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 var resortId = ["20", "25", "36", "77", "113", "120", "1673", "143", "181", "197", "220", "240", "329", 
 "330", "1435", "372", "425", "445", "456", "482", "507", "511"]
 
-var resortZip = 80241
+var resortZip = 80435;
 $("#test").on("click", function() {
     
     var openWeatherQueryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=" + resortZip + ",us&units=imperial&APPID=8945840c8250c919b4821b938074f3a6"
@@ -12,10 +12,13 @@ $("#test").on("click", function() {
         method: "GET"
     })
     .then(function(response) {
-        console.log(response);
-
+        var weatherArray = (response.list);
+        console.log(weatherArray[5]);
+        console.log(weatherArray[5].main.temp);
+        console.log(weatherArray[5]);
     })
 }) 
+
 /* Arapahoe Basin 20, Aspen Snowmass 25, Aspen Highlands 25, Aspen Mountain 25, Beaver Creek 36, Breckenridge 77, Buttermilk 25, 
 Copper 113, Crested Butte 120, echo mountain 1673, Eldora 143, GRANBY NO RESULT, Howelson 181, Keystone 197, Loveland 220, 
 Monarch Mtn 240, PowderHorn 329, Purgatory 330, Silverton 1435, Ski Cooper 372, Steamboat 425, Sunlight Mtn 445, Telluride 456, 
